92ec8a0184b7f08884b5f99df8cbcd5e6bf2e672
Everlasting12 <sidheshparab34@gmail.com>
2022-08-07 15:31:39 +0530

    polymorphism and counterClosure files added

---
+++ counterClosure.js
@@ -0,0 +1,31 @@
+/*
+let count = 0;
+window.onload = function () {
+  let button = document.getElementById("clickme");
+  button.onclick = handleClick;
+};
+function handleClick() {
+  let message = "You clicked me ";
+  let div = document.getElementById("message");
+  count++;
+  div.innerHTML = message + count + " times!";
+}
+*/
+
+
+
+function getElement(id) {
+  return document.getElementById(id);
+}
+
+window.onload = IncrementCountenWhenCliked;
+function IncrementCountenWhenCliked() {
+  let counter = 0;
+  let message = "You clicked me ";
+  let div = getElement("message");
+  let btn = getElement("clickme");
+  btn.onclick = () => {
+    counter += 1;
+    div.innerText = `${message} ${counter} times.`;
+  };
+}
---
+++ polymorphism.js
@@ -0,0 +1,43 @@
+class Shape {
+  constructor(input) {
+    this.input = input;
+  }
+  areaof() {
+    return "Area of";
+  }
+}
+
+class Circle extends Shape {
+  constructor(input) {
+    super(input);
+  }
+  area() {
+    console.log(`${this.areaof()} Cricle: ${3.14 * this.input * this.input}`);
+  }
+}
+
+class Square extends Shape {
+  constructor(input) {
+    super(input);
+  }
+  area() {
+    console.log(`${this.areaof()} Square: ${this.input * this.input}`);
+  }
+}
+class Rectangle extends Shape {
+  constructor(input, breadth) {
+    super(input);
+    this.breadth = breadth;
+  }
+  area() {
+    console.log(`${this.areaof()} Rectangle: ${this.input * this.breadth}`);
+  }
+}
+
+let circle = new Circle(7);
+let sqr = new Square(7);
+let rect = new Rectangle(8, 9);
+
+circle.area();
+sqr.area();
+rect.area();
